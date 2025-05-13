03b19264ca3f6eebb05d541e51d58e08e4b4883d
Jonas <jnsmarte@gmail.com>
2025-05-13 08:43:58 +0200

    base creation classes

--- SteamNewsletter/SteamNewsletterLib/GameCollection.cs
+++ SteamNewsletter/SteamNewsletterLib/GameCollection.cs
@@ -8,6 +8,21 @@ namespace SteamNewsletterLib
 {
     public class GameCollection
     {
+        public List<Game> Games { get; set; } = new List<Game>();
 
+        public void AddGame(Game game)
+        {
+            Games.Add(game);
+        }
+
+        public void RemoveGame(Game game)
+        {
+            Games.Remove(game);
+        }
+
+        public void ClearGames()
+        {
+            Games.Clear();
+        }
     }
 }
