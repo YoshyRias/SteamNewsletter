28cca071c05cb637100155a16c402c00430ba1af
Jonas <jnsmarte@gmail.com>
2025-05-14 16:08:21 +0200

    New Page -> Start Classes

---
+++ SteamNewsletter/SteamNewsletterLib/TrendGameCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SteamNewsletterLib
+{
+    public class TrendGameCollection
+    {
+        private List<Game> games = new();
+        private ListView listView;
+        private Grid grid;
+
+        public TrendGameCollection( Grid grid)
+        {
+            this.grid = grid;
+        }
+
+        public void UpdateListView()
+        {
+            //TODO: Update the ListView with the games from the collection
+        }
+
+        public void GetGames()
+        {
+            //TODO: Get games from SteamDB 
+        }
+
+    }
+}
